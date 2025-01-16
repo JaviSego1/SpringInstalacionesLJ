7a5fda30c8d8997b21f96d0ea5159d490af7dff3
juangualberto <juangu@ujaen.es>
2025-01-16 08:55:55 +0100

    terminado servicio instalacion

--- src/main/java/com/iesvdc/acceso/pistasdeportivas/servicios/ServiInstalacion.java
+++ src/main/java/com/iesvdc/acceso/pistasdeportivas/servicios/ServiInstalacion.java
@@ -2,22 +2,25 @@ package com.iesvdc.acceso.pistasdeportivas.servicios;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.iesvdc.acceso.pistasdeportivas.modelos.Instalacion;
 import com.iesvdc.acceso.pistasdeportivas.repos.RepoInstalacion;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -35,39 +38,56 @@ public class ServiInstalacion {
     }
 
     @GetMapping("/instalacion/{id}")
-    public Instalacion findOne(
+    public ResponseEntity<Instalacion> findOne(
         @PathVariable @NonNull Long id) {
         
         Optional<Instalacion> oInstalacion = repoInstalacion.findById(id);
         if (oInstalacion.isPresent()) {
-            return oInstalacion.get();
+            return ResponseEntity.ok().body(oInstalacion.get());
         } else {
-            throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Instalacion no encontrada");
-           
+            return ResponseEntity.notFound().build();
         }
     }
     
     @DeleteMapping("/instalacion/{id}")
-    public Instalacion deleteOne(
+    public ResponseEntity<Instalacion> deleteOne(
         @PathVariable @NonNull Long id) {
         
         Optional<Instalacion> oInstalacion = repoInstalacion.findById(id);
         if (oInstalacion.isPresent()) {
             repoInstalacion.delete(oInstalacion.get());
-            return oInstalacion.get();
+            return ResponseEntity.ok().body(oInstalacion.get());
         } else {
-            throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "Instalacion no encontrada");
-           
+            return ResponseEntity.notFound().build();
         }
     }
 
+
     @PostMapping("/instalacion")
-    public Instalacion create(@RequestBody Instalacion instalacion) {
-        return repoInstalacion.save(instalacion);
+    public ResponseEntity<Instalacion> create(@RequestBody Instalacion instalacion) 
+        throws URISyntaxException {
+
+        Instalacion i = repoInstalacion.save(instalacion);
+        return ResponseEntity.
+            created(new URI("/api/instalacion/"+i.getId())).
+            body(i);
     }
     
-    
+    @PutMapping("/instalacion/{id}")
+    public ResponseEntity<Instalacion> putInstalacion
+        (@PathVariable Long id, @RequestBody Instalacion instalacion) {
+        
+        Optional<Instalacion> oInstalacion = repoInstalacion.findById(id);
+        if (oInstalacion.isPresent()) {
+            Instalacion oldInstalacion = oInstalacion.get();
+            oldInstalacion.setNombre(instalacion.getNombre());
+            repoInstalacion.save(oldInstalacion);            
+            return ResponseEntity.ok().body(oInstalacion.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+        
+    }
+
     
 }
